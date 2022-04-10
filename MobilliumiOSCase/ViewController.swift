021898f2b85b25800da956d96b5a1c0e758a8631
Said ÇANKIRAN <cank32cank@gmail.com>
2022-04-10 16:22:40 +0300

    Project created.

---
+++ MobilliumiOSCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobilliumiOSCase
+//
+//  Created by Said Çankıran on 10.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
